ca6777b1c863b053d0304b1e77760ae4a3e7a04f
satyan69 <36621335+satyan69@users.noreply.github.com>
2020-07-26 18:51:58 +0530

    js and css files added

---
+++ js/script.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+
+    var list = document.getElementById("shuffleAndSort");
+    /*==========  Method to Shuffle the Data  ==========*/
+    const shuffleData = (items) => {
+        let shallowCopy = items.slice(0), i = shallowCopy.length;
+        while (--i) {
+            const rand = Math.floor(Math.random() * i);
+            [shallowCopy[rand], shallowCopy[i]] = [shallowCopy[i], shallowCopy[rand]];
+        }
+        return shallowCopy;
+    }
+    /*==========  Method to Shuffle the Node  ==========*/
+    const shuffleNodes = () => {
+        let i = 0;
+        let nodes = shuffleData([...list.children]);
+        let nodeLength = nodes.length;
+        while (i < nodeLength) {
+            list.appendChild(nodes[i]);
+            ++i;
+        }
+    }
+    /*==========  End  ==========*/
+    /*==========  Method to Sort the Node  ==========*/
+    const sortNode = () => {
+        [...list.children]
+            .sort((i, j) => i.innerText > j.innerText ? 1 : -1)
+            .forEach(node => list.appendChild(node))
+    }
+    /*==========  End  ==========*/
+
+    document.getElementById("shuffleNode").onclick = shuffleNodes;
+    document.getElementById("sortNode").onclick = sortNode;
+}
